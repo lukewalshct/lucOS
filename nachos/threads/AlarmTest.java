ae36ee2b47a488c89aef1815b73466d61b87b285
CentosOS6.4 <cloudera@localhost.localdomain>
2017-03-23 18:40:51 -0700

    added call to waitUntil in AlarmTest.java; implementation working correctly but uses busy waiting (need to update to properly use thread sleep)

--- nachos/threads/AlarmTest.java
+++ nachos/threads/AlarmTest.java
@@ -12,11 +12,13 @@ public class AlarmTest {
 
 	public void TestWaitUntil()
 	{
+		Alarm alarm = new Alarm();
+		
 		KThread t1 = new KThread(new Runnable() { 
-			public void run() { AssertWaitTimeIsCorrect(10000); } 
+			public void run() { AssertWaitTimeIsCorrect(alarm, 10000); } 
 			});
 		KThread t2 = new KThread(new Runnable() { 
-			public void run() { AssertWaitTimeIsCorrect(30000); } 
+			public void run() { AssertWaitTimeIsCorrect(alarm, 30000); } 
 			});
 		
 		t1.setName("ALARM TEST Thread 1");
@@ -32,13 +34,27 @@ public class AlarmTest {
 		}catch(Exception e){}
 	}
 	
-	private void AssertWaitTimeIsCorrect(long clockCycles)
+	/*
+	 * Calls the alarm to wait the specified clock cycles, and then 
+	 * asserts that the correct amount of cycles (or more) have passed.
+	 */
+	private void AssertWaitTimeIsCorrect(Alarm alarm, long clockCycles)
 	{
+		
 		String tName = KThread.currentThread().getName();
 		
-		long curTime = Machine.timer().getTime();
+		long curTime1 = Machine.timer().getTime();
+		
+		//wait
+		alarm.waitUntil(clockCycles);
+		
+		long curTime2 = Machine.timer().getTime();
+		
+		//assert the waitin was correct (>= clockcycles requested to wait)
+		Lib.assertTrue((curTime2 - curTime1) >= clockCycles);
 		
 		System.out.println(tName + "\nWait requested (clock cycles): " +
-			clockCycles + "\nCurrent time (ticks since machine boot): " + curTime);
+			clockCycles + "\nTime before wait: " + curTime1 +
+			"\nTime after wait: " + curTime2 + "\nTime elapsed: " + (curTime2 -curTime1));
 	}
 }
