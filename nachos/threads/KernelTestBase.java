7b9b8e50cec5e015e8bc7f6f0187b25c5e9c0038
Luke Walsh <lwalsh@kgsbuildings.com>
2017-04-14 12:16:33 -0400

    added test thread creation in lock test for priority scheduler; updated kerneltestbase to disable interrupts while forking test threads

--- nachos/threads/KernelTestBase.java
+++ nachos/threads/KernelTestBase.java
@@ -1,5 +1,7 @@
 package nachos.threads;
 
+import nachos.machine.*;
+
 /*
  * Contains some common/utility methods common to several
  * Kernel test classes.
@@ -13,10 +15,14 @@ public class KernelTestBase {
 	{
 		if(threads == null) return;
 		
+		boolean intStatus = Machine.interrupt().disable();
+		
 		for(int i = 0; i < threads.length; i++)
 		{
 			threads[i].fork();
 		}
+		
+		Machine.interrupt().restore(intStatus);
 	}
 	
 	/*
--- nachos/threads/PrioritySchedulerTest.java
+++ nachos/threads/PrioritySchedulerTest.java
@@ -4,15 +4,76 @@ package nachos.threads;
  * 
  * TODO: add documentation
  */
-public class PrioritySchedulerTest {
+public class PrioritySchedulerTest extends KernelTestBase {
+	
+	private final int NUM_PRIORITIES = 8;
 	
 	public void RunTests()
 	{
 		System.out.println("\n\nRunning PriorityScheduler tests...\n");
 		
-		//TODO: add tests
+		PriorityScheduler scheduler = (PriorityScheduler) ThreadedKernel.scheduler;
+		
+		runLockTest(scheduler, 20);
 		
 		System.out.println("\n\nPriorityScheduler tests complete.\n");
+	}
+	
+	private void runLockTest(PriorityScheduler scheduler, int numThreads)
+	{
+		System.out.println("entering lock test");
+		
+		Lock lock = new Lock();
+		
+		KThread[] testThreads = new KThread[numThreads];
+		
+		for(int i = 0; i < numThreads; i++)
+		{
+			testThreads[i] = new KThread(new TestThread(i, lock, i % NUM_PRIORITIES));
+		}
+
+		//fork all threads
+		runThreads(testThreads);
+		
+		try{
+			joinThreads(testThreads);
+		}catch(Exception ex) {}
+		
+		System.out.println("exiting lock test");
+	}
+	
+	private class TestThread implements Runnable
+	{
+		private int _priority;
+		
+		private int _threadNum;
+		
+		private Lock _lock;
+		
+		public TestThread(int threadNum, Lock lock, int priority)
+		{
+			_threadNum = threadNum;
+			
+			_lock = lock;
+			
+			_priority = priority;
+		}
+		
+		public void run()
+		{
+			GetLock();
+		}
+		
+		public void GetLock()
+		{
+			_lock.acquire();
+			
+			System.out.println("Thread #" + _threadNum + " acquired lock. Priority is " + _priority);
+			
+			_lock.release();
+		}
+		
+		
 	}
 
 }
