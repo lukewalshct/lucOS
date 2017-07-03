92e6ea35ddbb3856f6e67878e391cdd091314a89
Luke Walsh <lukewalshct@gmail.com>
2017-07-03 14:17:52 -0400

    added tests to read() syscall C test prog

--- nachos/test/readTest.c
+++ nachos/test/readTest.c
@@ -5,20 +5,30 @@
 
 #include "syscall.h"
 
-int testRead(char* fName, char* buffer, int size, char* expected);
+int testRead(char* fName, int bufferLen, int size, char* expected);
 
-void verifyResult(char* buffer, char* expected);
+void verifyResult(char* buffer, char* expected, int bytesRead);
 void throwReadError();
 
 int main()
 {
-    char buffer[10];
+    //normal read from a file
+    testRead("rTest1.txt", 10, 10, "0123456789");
 
-    testRead("rTest1.txt", buffer, 10, "0123456789");
+    //read from a file w/text, but size 0 - should read 0
+    testRead("rTest1.txt", 0, 0, ""); 
+
+    //read from file w/ no text - shoud read 0
+    testRead("rTest2.txt", 10, 10, "");
+
+    //read from file buffer smaller than read size - should
+    testRead("rTest1.txt", 5, 10, "0123456789");
 }
 
-int testRead(char *fName, char* buffer, int size, char* expected)
+int testRead(char *fName, int bufferLen, int size, char* expected)
 {
+    char buffer[bufferLen];
+
     int fHandle = open(fName);
 
     if(fHandle == -1)
@@ -29,18 +39,18 @@ int testRead(char *fName, char* buffer, int size, char* expected)
     }
     else
     {
-        int bytesRead = read(fHandle, buffer, 10);
+        int bytesRead = read(fHandle, buffer, size);
 
         close(fHandle);
 
-        verifyResult(buffer, expected);
+        verifyResult(buffer, expected, bytesRead);
 
         return bytesRead;
     }
 
 }
 
-void verifyResult(char* buffer, char* expected)
+void verifyResult(char* buffer, char* expected, int read)
 {
     printf("\nRead test expected result:\n");
 
@@ -53,8 +63,9 @@ void verifyResult(char* buffer, char* expected)
     printf("\n");
 
     int i;
- 
-    if(strlen(expected) != strlen(buffer)) throwReadError();
+
+    if(strlen(expected) != strlen(buffer) &&
+        (strlen(expected) != read)) throwReadError();
 
     for(i = 0; i < strlen(expected); i++)
     {
