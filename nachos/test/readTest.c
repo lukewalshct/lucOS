504eccd17674ef65b9ed27ddf24bf4fe9d7357c5
Luke Walsh <lukewalshct@gmail.com>
2017-07-03 11:26:48 -0400

    updated read() syscall C test program to compare expected vs actual read results

--- nachos/test/readTest.c
+++ nachos/test/readTest.c
@@ -5,16 +5,19 @@
 
 #include "syscall.h"
 
-int testRead(char* fName, char* buffer, int size);
+int testRead(char* fName, char* buffer, int size, char* expected);
+
+void verifyResult(char* buffer, char* expected);
+void throwReadError();
 
 int main()
 {
-    char buffer[100];
+    char buffer[10];
 
-    testRead("rTest1.txt", buffer, 10);
+    testRead("rTest1.txt", buffer, 10, "0123456789");
 }
 
-int testRead(char *fName, char* buffer, int size)
+int testRead(char *fName, char* buffer, int size, char* expected)
 {
     int fHandle = open(fName);
 
@@ -30,9 +33,39 @@ int testRead(char *fName, char* buffer, int size)
 
         close(fHandle);
 
-        printf(buffer);
+        verifyResult(buffer, expected);
 
         return bytesRead;
     }
 
+}
+
+void verifyResult(char* buffer, char* expected)
+{
+    printf("\nRead test expected result:\n");
+
+    printf(expected);
+    
+    printf("\nRead test actual result:\n");
+
+    printf(buffer);
+
+    printf("\n");
+
+    int i;
+ 
+    if(strlen(expected) != strlen(buffer)) throwReadError();
+
+    for(i = 0; i < strlen(expected); i++)
+    {
+        if(buffer[i] != expected[i]) throwReadError();        
+    }     
+
+}
+
+void throwReadError()
+{
+    printf("!!!!READ TEST ERROR!!!");
+
+    halt();
 }
