556626e8452dab3eac9b6770a379d6a6729487ef
Luke Walsh <lukewalshct@gmail.com>
2017-07-03 14:36:04 -0400

    added test to read() syscall C prog to check invalid read buffer mem address

--- nachos/test/readTest.c
+++ nachos/test/readTest.c
@@ -5,31 +5,33 @@
 
 #include "syscall.h"
 
-int testRead(char* fName, char* buffer, int size, char* expected);
-
-void verifyResult(char* buffer, char* expected, int bytesRead);
+int testRead(char* fName, char* buffer, int size, char* expected, int compare);
+void verifyResult(char* buffer, char* expected, int bytesRead, int compare);
 void throwReadError();
 
 int main()
 {
     //normal read from a file
     char buff1[10];
-    testRead("rTest1.txt", buff1, 10, "0123456789");
+    testRead("rTest1.txt", buff1, 10, "0123456789", 1);
 
     //read from a file w/text, but size 0 - should read 0
     char buff2[0];
-    testRead("rTest1.txt", buff2, 0, ""); 
+    testRead("rTest1.txt", buff2, 0, "", 1); 
 
     //read from file w/ no text - shoud read 0
     char buff3[10];
-    testRead("rTest2.txt", buff3, 10, "");
+    testRead("rTest2.txt", buff3, 10, "", 1);
 
     //read from file buffer smaller than read size - should
     char buff4[5];
-    testRead("rTest1.txt", buff4, 10, "0123456789");
+    testRead("rTest1.txt", buff4, 10, "0123456789", 1);
+
+    //read from vaddress buffer that is out of virtual mem
+    testRead("rTest1.txt", 9999999, 10, "", 0);
 }
 
-int testRead(char *fName, char *buffer, int size, char* expected)
+int testRead(char *fName, char *buffer, int size, char* expected, int compare)
 {
     int fHandle = open(fName);
 
@@ -45,15 +47,17 @@ int testRead(char *fName, char *buffer, int size, char* expected)
 
         close(fHandle);
 
-        verifyResult(buffer, expected, bytesRead);
+        verifyResult(buffer, expected, bytesRead, compare);
 
         return bytesRead;
     }
 
 }
 
-void verifyResult(char* buffer, char* expected, int read)
+void verifyResult(char* buffer, char* expected, int read, int compare)
 {
+    if(!compare) return;
+
     printf("\nRead test expected result:\n");
 
     printf(expected);
