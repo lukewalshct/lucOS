d387f2f35f43a379c370ba82242229d0da7d7399
Luke Walsh <lukewalshct@gmail.com>
2017-07-17 08:35:51 -0400

    added simple hello world test C program

---
+++ nachos/test/simpleHello.c
@@ -0,0 +1,11 @@
+/*  simpleHello.c
+ *	Program that prints out hello world
+ * 	plus a small message. Useful for testing
+ * 	opening/closing new processes in lucOS.
+ */
+
+int main()
+{
+    printf("Hello, world!\n");
+}
+
