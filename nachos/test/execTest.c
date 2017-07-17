68c8bb14d7c298db9094521fc073c02ced6aa497
Luke Walsh <lukewalshct@gmail.com>
2017-07-17 18:12:53 -0400

    updated exec() syscall test C program to create and destroy multiple processes

--- nachos/test/execTest.c
+++ nachos/test/execTest.c
@@ -3,15 +3,28 @@
  * 	by creating new processes.
  */
 
+int NUM_PROGRAMS = 5;
+
 int main()
 {
     char *file = "simpleHello.coff";
-
+   
     int argc = 0;
 
     char *argv[] = {};
 
-    int newPID1 = exec(file, argc, argv);
+    int prog_cnt;
 
-    printf("process created, pid is %d", newPID1);
+    for(prog_cnt = 0; prog_cnt < NUM_PROGRAMS; prog_cnt++)
+    {
+   
+        int newPID1 = exec(file, argc, argv);
+
+        //Add slight delay to printing success message
+        int dummy = 1;
+
+        while(dummy++ < 9999);
+
+        printf("process created, pid is %d\n", newPID1);
+    }
 }
